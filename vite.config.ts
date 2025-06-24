49e2bed5bfafeeef639e317a627150f81271ce3a
hoanghiep2625 <hoanghiep262005@gmail.com>
2025-06-25 01:20:45 +0700

    update config vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: "0.0.0.0",
+    port: 5174,
+    allowedHosts: ["admin.elavia.tahoanghiep.com"],
+  },
 });
